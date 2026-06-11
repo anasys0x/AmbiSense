e8a682d26ac68a49b5643ba6b0943240124d3ffa
Anas Mrani <vendettacmlv@gmail.com>
2026-06-11 02:40:41 -0400

    feat: POST /measurements avec auth

---
+++ src/routes/measurements.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middlewares/auth');
+const Measurement = require('../models/Measurement');
+const Device = require('../models/Device');
+
+router.post('/measurements', auth, async (req, res) => {
+    try{
+        const { type, value, location , timestamp} = req.body;
+        if (!type || value === undefined || !location || !timestamp) {
+            return res.status(400).json("Champs 'type', 'value' ou 'location' manquant !");
+        }
+        const measurement = new Measurement({ type, value, location, timestamp, deviceId: req.device._id })
+        await measurement.save();
+        res.status(201).json(measurement);
+
+    } catch(err) {
+        res.status(400).send(err)
+    }
+})
+
+module.exports = router;
