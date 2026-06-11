d96d1d421f32fd376794117d9331668bdac92c8c
Anas Mrani <vendettacmlv@gmail.com>
2026-06-11 03:41:32 -0400

    feat: route POST /observations avec auth

---
+++ src/routes/observations.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middlewares/auth');
+const Observation = require('../models/Observation');
+
+router.post('/observations', auth, async (req, res) => {
+    try{
+        const { location, proximity, vibe, notes } = req.body;
+        if (!location || !proximity || !vibe || notes === undefined) {
+            return res.status(400).send("Champs 'location', 'proximity', vibe ou 'notes' manquant !")
+        }
+        const observations = new Observation({ location, proximity, vibe, notes });
+        await observations.save();
+        res.status(201).json(observations);
+
+    } catch(err){
+      res.status(400).send(err.message);
+    }
+})
+
+module.exports = router;
