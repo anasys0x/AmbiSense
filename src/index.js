e744385631afa9f6c233d8b54a0bab7be690eaa2
NizarLakhder <lakhder.nizar@outlook.com>
2026-06-17 03:12:44 -0500

    chore: réintégration du middleware cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./services/mongoose');
 
 
@@ -12,6 +13,7 @@ connectDB().catch((err) => {
 });
 
 // Mes branchements
+app.use(cors());
 app.use(express.json());
 app.use(require('./routes/devices'));
 app.use(require('./routes/measurements'));
